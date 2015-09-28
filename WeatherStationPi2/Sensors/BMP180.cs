26ed590aedbeacb18b6a4cd3ba89317884bad2de
Dmitri Soshnikov <dmitri@soshnikov.com>
2015-09-28 13:54:26 +0300

    Added WeatherStationPi2 Project

---
+++ WeatherStationPi2/Sensors/BMP180.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeatherStationPi2.Sensors
+{
+    // Draft version based on the code from http://www.guruumeditation.net/en/io-ic-with-windows-10-iot-raspberry-pi-2-and-the-temperature-sensor-bmp180/
+    public class BMP180 : I2CDevice
+    {
+        public double Temperature
+        {
+            get
+            {
+                var mre = new ManualResetEventSlim(false);
+                Write(_bmp085RegisterControl, _bmp085RegisterReadtempcmd);
+                mre.Wait(TimeSpan.FromMilliseconds(5));
+                var t = (int)ReadUInt16(_bmp085RegisterTempdata);
+
+                var b5 = ComputeB5(t);
+                t = (b5 + 8) >> 4;
+                var temp = t / 10.0;
+                return temp;
+            }
+        }
+
+        private ushort _ac5;
+        private ushort _ac6;
+        private short _mc;
+        private short _md;
+        private int ComputeB5(int value)
+        {
+            var x1 = (value - _ac6) * _ac5 >> 15;
+            var x2 = (_mc << 11) / (x1 + _md);
+            return x1 + x2;
+        }
+
+        public override async Task Init(int n = 0)
+        {
+            await base.Init(n);
+            _ac5 = ReadUInt16(_bmp085RegisterCalAc5);
+            _ac6 = ReadUInt16(_bmp085RegisterCalAc6);
+            _mc = ReadInt16(_bmp085RegisterCalMc);
+            _md = ReadInt16(_bmp085RegisterCalMd);
+        }
+
+        private byte _bmp085RegisterCalAc5 = 0xB2; // R   Calibration data (16 bits)
+        private byte _bmp085RegisterCalAc6 = 0xB4; // R   Calibration data (16 bits)
+        private byte _bmp085RegisterCalMc = 0xBC; // R   Calibration data (16 bits)
+        private byte _bmp085RegisterCalMd = 0xBE; // R   Calibration data (16 bits) 
+
+        private byte _bmp085RegisterControl = 0xF4;
+        private byte _bmp085RegisterTempdata = 0xF6;
+
+        private byte _bmp085RegisterReadtempcmd = 0x2E;
+
+
+
+    }
+}
---
+++ WeatherStationPi2/Sensors/I2CDevice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace WeatherStationPi2.Sensors
+{
+    // First draft version of a class representing I2C Device
+    // Based on the code from http://www.guruumeditation.net/en/io-ic-with-windows-10-iot-raspberry-pi-2-and-the-temperature-sensor-bmp180/
+    public class I2CDevice
+    {
+
+        public I2CDevice()
+        {
+
+        }
+
+        public virtual async Task Init(int n=0)
+        {
+            var i2CSettings = new I2cConnectionSettings(0x77)
+            {
+                BusSpeed = I2cBusSpeed.FastMode,
+                SharingMode = I2cSharingMode.Shared
+            };
+            var i2c1 = I2cDevice.GetDeviceSelector("I2C1");
+            var devices = await DeviceInformation.FindAllAsync(i2c1);
+            sensor = await I2cDevice.FromIdAsync(devices[n].Id, i2CSettings);
+        }
+
+        protected I2cDevice sensor;
+
+        public short ReadInt16(byte register)
+        {
+            var value = new byte[2];
+
+            sensor.WriteRead(new[] { register }, value);
+
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(value);
+
+            return BitConverter.ToInt16(value, 0);
+        }
+
+        public ushort ReadUInt16(byte register)
+        {
+            var value = ReadInt16(register);
+
+            return (ushort)value;
+        }
+
+        public void Write(byte register, byte command)
+        {
+            sensor.Write(new[] { register, command });
+        }
+
+    }
+}
